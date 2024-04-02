4aba08cb50827ba427a5244c84d37d1c31e89cbd
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-04-02 09:52:41 +0100

    updated test to use OsmUserFactory

--- backend/tests/test_login_model.py
+++ backend/tests/test_login_model.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from login.models import OsmUser
+
+from .factories import OsmUserFactory
 
 
 class TestLoginModels(TestCase):
 
     def test_OsmUser_creation(self):
-        osm_user = OsmUser.objects.create(username="Test User", osm_id=123456)
+        osm_user = OsmUserFactory()
 
         self.assertEqual(str(osm_user), "Test User")
         self.assertEqual(osm_user.osm_id, 123456)
